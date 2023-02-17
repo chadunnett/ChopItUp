5fabfd51fbc33b633198db920bc4c2d41c7fa9bb
derek helgoe <derek1896@gmail.com>
2023-02-16 19:49:45 -0600

    utils and post form

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,83 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_POST = gql`
+  mutation addThought($postText: String!) {
+    addPost(postText: $postText) {
+      _id
+      postText
+      createdAt
+      username
+      reviews
+      comments {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentBody: String!) {
+    addComment(postId: $postId, commentBody: $commentBody) {
+      _id
+      commentBody
+      username
+      createdAt 
+}
+`;
+
+export const ADD_REVIEW = gql`
+  mutation addReview($postId: ID!, $reviews: Float!) {
+    addReview(postId: $postId, reviews: $reviews) {
+      _id
+}
+`;
+
+
+export const REMOVE_POST = gql`
+  mutation removePost($id: ID!) {
+    removePost(id: $id) {
+      _id
+    }
+  }
+`;
+
+export const REMOVE_COMMENT = gql`
+  mutation removeComment($postId : ID!, $commentId: ID!) {
+    removeComment($postId : ID!, $commentId: ID!) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_COMMENT = gql`
+  mutation updateComment($Id : ID!, $commentBody: String!) {
+    updateComment($Id : ID!, $commentBody: String!) {
+      _id
+      commentBody
+    }
+  }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,99 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_POSTS = gql`
+  query posts($username: String) {
+    posts(username: $username) {
+      _id
+      postText
+      createdAt
+      username
+      reviews
+      comments {
+        _id
+        createdAt
+        username
+        commentBody
+      }
+    }
+  }
+`;
+
+export const QUERY_POST = gql`
+  query post($id: ID!) {
+    post(_id: $id) {
+        _id
+      postText
+      createdAt
+      username
+      reviews
+      comments {
+        _id
+        createdAt
+        username
+        commentBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    
+      post {
+        _id
+        postText
+        createdAt
+        reviews
+        comments {
+        _id
+        }
+        
+      }
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+  query users($username: String!) {
+    users(username: $username) {
+      _id
+      username
+      email
+    
+      post {
+        _id
+        postText
+        createdAt
+        reviews
+        comments {
+        _id
+        }
+        
+      }
+    }
+  }
+`;
+export const QUERY_ME = gql`
+
+  {
+    me {
+      _id
+      username
+      email
+    
+      post {
+        _id
+        postText
+        createdAt
+        reviews
+        comments {
+        _id
+        }
+    }
+  }
+`;
+
