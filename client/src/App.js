a65840eaa24840345ece15ee8144ea9599b7071d
druharo <111248621+druharo@users.noreply.github.com>
2023-02-20 13:21:04 +0300

    Added Mutations, views on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from 'react-bootstrap/Col';
@@ -10,10 +8,10 @@ import { useState } from "react";
 import Row from 'react-bootstrap/Row';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-
-
 import "./App.css";
 import Home from "./views/Home";
+import Login from "./views/Login";
+import Layout from "./views/Layout";
 
 export default function App() {
   return (
@@ -35,38 +33,6 @@ export default function App() {
   );
 }
 
-function Layout() {
-  return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            ChopItUp
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/profile">
-              Profile
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-            <Nav.Link as={Link} to="/signup">
-              Sign Up
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-    </div>
-  );
-}
-
 function Profile() {
   return (
     <div>
@@ -75,18 +41,18 @@ function Profile() {
   );
 }
 
-function SignUp()  {
-    const [validated, setValidated] = useState(false);
-  
-    const handleSubmit = (event) => {
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  
-      setValidated(true);
-    };
+function SignUp() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
 
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
@@ -159,31 +125,6 @@ function SignUp()  {
         />
       </Form.Group>
       <Button type="submit">Submit form</Button>
-    </Form>
-  );
-}
-
-function Login() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
     </Form>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -19,15 +19,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
+import { setContext } from '@apollo/client/link/context';
 import './index.css';
 import './custom.scss';
 import App from './App';
 
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+// 3
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+// 4
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ApolloProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
