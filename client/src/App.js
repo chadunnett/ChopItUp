6cbabcd62d374e8f9a3968b0e5ea5a22ec08f308
druharo <111248621+druharo@users.noreply.github.com>
2023-02-23 00:28:19 +0300

    changes2

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ import Profile from './views/Profile'
 import Layout from "./views/Layout";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
